4340b7bca2a01ae9d54c457dc547e1eec81b664b
택택 <ghxor32@icloud.com>
2026-06-24 15:19:13 +0900

    3333

--- day8_2_login.py
+++ day8_2_login.py
@@ -10,7 +10,7 @@ app.add_middleware(
 
     allow_origins=["*"],     # React 개발 서버만 허용(허용할 출처)
 
-    allow_credentials=True,  #(인증정보 허용, 쿠키, jWT토큰 등등)
+    allow_credentials=False,  #(인증정보 허용, 쿠키, jWT토큰 등등)
 
     allow_methods=["*"],     #허용할 HTTP메서드(get, post, patch, put, delete)
 
